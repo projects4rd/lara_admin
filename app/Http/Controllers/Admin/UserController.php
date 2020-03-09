e07f357fab769e370fd779cec637c43fcd44064d
Rob Dorn <projects4rd@gmail.com>
2020-03-09 19:32:09 +0100

    Adding Admin Controllers and local models

---
+++ app/Http/Controllers/Admin/UserController.php
@@ -0,0 +1,114 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Role;
+use App\User;
+use App\Permission;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $result = User::latest()->paginate();
+        return view('user.index', compact('result'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        $roles = Role::pluck('name', 'id');
+        return view('user.new', compact('roles'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'name' => 'bail|required|min:2',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|min:6',
+            'roles' => 'required|min:1'
+        ]);
+
+        $request->merge(['password' => bcrypt($request->get('password'))]);
+
+        if ($user = User::create($request->except('roles', 'permissions'))) {
+            $this->syncPermissions($request, $user);
+            $alert = ['type' => 'success', 'message' => 'Succesfully created user'];
+        } else {
+            $alert = ['type' => 'error', 'message' => 'Unable to create user'];
+        }
+
+        return redirect()->rout('users.index')->with($alert['type'], $alert['message']);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $user = User::find($id);
+        $roles = Role::pluck('name', 'id');
+        $permissions = Permission::all('name', 'id');
+
+        return view('user.edit', compact('user', 'roles', 'permissions'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $this->validate($request, [
+            'name' => 'bail|required|min:2',
+            'email' => 'required|email|unique:users, email' . $id,
+            'roles' => 'required|min:1'
+        ]);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
--- database/seeds/RolesAndPermissionsSeeder.php
+++ database/seeds/RolesAndPermissionsSeeder.php
@@ -12,28 +12,31 @@ class RolesAndPermissionsSeeder extends Seeder
         app()[\Spatie\Permission\PermissionRegistrar::class]->forgetCachedPermissions();
 
         // create permissions
-		$permissions = [
-			'list-role',
-			'create-role',
-			'edit-role',
-			'delete-role',
-			'edit-post',
-			'delete-post',
-			'publish-post',
-			'unpublish-post'
-		];
-		
-		foreach ($permissions as $permission){		
-			Permission::create(['name' => $permission]);
-		}
+        $permissions = [
+            'list-users',
+            'add-users',
+            'edit-users',
+            'delete-users',
+            'list-roles',
+            'create-roles',
+            'edit-roles',
+            'delete-roles',
+            'edit-posts',
+            'delete-posts',
+            'publish-posts',
+            'unpublish-posts'
+        ];
+
+        foreach ($permissions as $permission) {
+            Permission::create(['name' => $permission]);
+        }
 
         // create roles and assign created permissions
 
         $role = Role::create(['name' => 'writer'])
-			->givePermissionTo('edit-post');
+            ->givePermissionTo('edit-posts');
 
         $role = Role::create(['name' => 'moderator'])
-            ->givePermissionTo(['publish-post', 'unpublish-post']);
-
+            ->givePermissionTo(['publish-posts', 'unpublish-posts']);
     }
 }
