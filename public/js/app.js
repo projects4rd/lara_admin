147b5533260b9eaf5a85d26cb055b0d50d4ceafb
R. Dorn <rdorn@simactriangle.com>
2020-05-05 12:55:18 +0200

    Correction on User Create menu

--- public/js/app.js
+++ public/js/app.js
@@ -49672,9 +49672,9 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! D:\code\laravel\lara_admin\resources\js\app.js */"./resources/js/app.js");
-__webpack_require__(/*! D:\code\laravel\lara_admin\resources\sass\app.scss */"./resources/sass/app.scss");
-module.exports = __webpack_require__(/*! D:\code\laravel\lara_admin\resources\sass\admin.scss */"./resources/sass/admin.scss");
+__webpack_require__(/*! D:\www\htdocs\lara_admin\resources\js\app.js */"./resources/js/app.js");
+__webpack_require__(/*! D:\www\htdocs\lara_admin\resources\sass\app.scss */"./resources/sass/app.scss");
+module.exports = __webpack_require__(/*! D:\www\htdocs\lara_admin\resources\sass\admin.scss */"./resources/sass/admin.scss");
 
 
 /***/ })
